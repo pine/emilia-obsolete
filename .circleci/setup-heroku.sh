608fdf7fa5ef94e60df7efb990f98b46f88d8f86
Pine Mizune <pinemz@gmail.com>
2017-10-08 14:56:34 +0900

    fix(setup-heroku.sh): fix repository url

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git remote add heroku https://git.heroku.com/terminus-est.git
+git remote add heroku https://git.heroku.com/pinemz-emilia.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
 tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
