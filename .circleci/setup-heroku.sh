3ded0cc7a16f3bf3e2cf966bd2c42a497467bc81
Pine Mizune <pinemz@gmail.com>
2017-10-08 14:42:06 +0900

    fix(circleci): remove verbose flag

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -3,7 +3,7 @@
 git remote add heroku https://git.heroku.com/terminus-est.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 mkdir -p /usr/local/lib /usr/local/bin
-tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+tar -xzf heroku-linux-amd64.tar.gz -C /usr/local/lib
 ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
